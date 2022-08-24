790a8d31a1674ec35e4315921d641c309daddd70
serhatbek <serhat.bekk@gmail.com>
2022-08-24 13:21:37 +0300

    show colors on screen

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,7 +1,19 @@
 import rgbToHex from './utils';
+import { useState } from 'react';
 
-const SingleColor = ({ rgb, weight, index }) => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bgc = rgb.join(',');
+  // const hex = rgbToHex(...rgb);
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bgc})` }}
+    >
+      <p className='percent-value'>{weight}</p>
+      <p className='color-value'>#{hexColor}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
